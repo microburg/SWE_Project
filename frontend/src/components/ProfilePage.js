3e670c5bb7bdce0e76f90cab9c536d32df00b92a
microburg <s-omar.hamed@zewailcity.edu.eg>
2024-12-19 21:51:47 +0200

    Updated the profile page

--- frontend/src/components/ProfilePage.js
+++ frontend/src/components/ProfilePage.js
@@ -45,16 +45,61 @@ const ProfilePage = () => {
   }
 
   return (
-    <div className="container">
-      <h1>Profile Information</h1>
-      <div className="form-container">
-        <h2>Personal Details</h2>
-        <label>Name:</label>
-        <input type="text" value={userData.username} readOnly />
+    // <div className="container">
+    //   <h1>Profile Information</h1>
+    //   <div className="form-container">
+    //     <h2>Personal Details</h2>
+    //     <label>Name:</label>
+    //     <input type="text" value={userData.username} readOnly />
 
-        <label>Email:</label>
-        <input type="email" value={userData.email} readOnly />
-      </div>
+    //     <label>Email:</label>
+    //     <input type="email" value={userData.email} readOnly />
+    //   </div>
+    // </div>
+    
+
+
+      <div class="account-overview">  
+        <h2>Account Overview</h2>  
+
+        <div className="form-row">
+          <div className="form-group">
+            <label htmlFor="first-name">First Name *</label>
+            <input type="text" id="first-name" placeholder="Enter your first name" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="last-name">Last Name *</label>
+            <input type="text" id="last-name" placeholder="Enter your last name" />
+          </div>
+        </div>
+
+
+        <div class="form-group">  
+            <label>Username</label>  
+            <input type="text" value={userData.username} readOnly placeholder="Username" />  
+        </div>  
+
+        <div class="form-group">  
+            <label>Email:</label>  
+            <input type="email" value={userData.email} readOnly placeholder="Email address" />  
+        </div>  
+
+        <div class="form-group">  
+            <label for="current-password">Current Password (leave blank to leave unchanged)</label>  
+            <input type="password" id="current-password" placeholder="Current password" />  
+        </div>  
+
+        <div class="form-group">  
+            <label for="new-password">New Password (leave blank to leave unchanged)</label>  
+            <input type="password" id="new-password" placeholder="New password" />  
+        </div>  
+
+        <div class="form-group">  
+            <label for="confirm-password">Confirm New Password</label>  
+            <input type="password" id="confirm-password" placeholder="Confirm new password" />  
+        </div>  
+
+        <button type="submit" class="btn-save">Save Changes</button>  
     </div>
   );
 };
