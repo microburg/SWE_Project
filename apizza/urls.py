609985f5b279d5f83a945716958281fb8b575f9c
rana7azem <ranahazem816@gmail.com>
2024-12-20 17:15:02 +0200

    test

--- apizza/urls.py
+++ apizza/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', lambda request: redirect('/admin/')),
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
