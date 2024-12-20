9ad92bb96d5698f5bed0abf4730eca592207caa4
M-Abdeltawab <s-mahmoud.twaab@zewailcity.edu.eg>
2024-12-20 20:49:25 +0200

    Updated Menu and Intgrated it with Cart

--- users/models.py
+++ users/models.py
@@ -27,10 +27,11 @@ class Payment(models.Model):
         return f"{self.service_type} - {self.amount} - {self.payment_status}"
 
 class Pizza(models.Model):  
-    id = models.CharField(max_length=10, primary_key=True) 
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)  
     description = models.TextField()  
-    price = models.DecimalField(max_digits=10, decimal_places=2)  
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='pizzas/', blank=True, null=True)
 
     def __str__(self):  
         return self.name 
@@ -53,14 +54,20 @@ class Cart(models.Model):
         return f"Cart for {self.user.username}"
 
 
-class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
-    topping = models.ForeignKey(Topping, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
+class CartItem(models.Model):  
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)  
+    topping = models.ForeignKey(Topping, null=True, blank=True, on_delete=models.CASCADE)   
+    pizza = models.ForeignKey(Pizza, null=True, blank=True, on_delete=models.CASCADE)     
+    quantity = models.PositiveIntegerField(default=1)  
 
-    def str(self):
-        return f"{self.quantity} x {self.topping.name}"
+    def __str__(self):  
+        item_name = self.pizza.name if self.pizza else self.topping.name if self.topping else 'Unknown Item'  
+        return f"{self.quantity} x {item_name}"  
 
-
-    def total_price(self):
-        return self.quantity * self.topping.price
+    def total_price(self):  
+        total_price = 0  
+        if self.pizza:  
+            total_price += self.quantity * self.pizza.price  
+        if self.topping:  
+            total_price += self.quantity * self.topping.price  
+        return total_price
